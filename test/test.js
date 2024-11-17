5af1ece2310614023d912c7de9c1799da1a70f47
Niyas-A <niyasttshr33@gmail.com>
2024-11-16 18:40:30 -0800

    Issue #8: Created jsdoc yaml and comment in test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
-function testAddition() {
+/** 
+ * This is a description of the testAddition function. 
+*/
+function testAddition(length, breadth) {
     if (1 + 1 !== 2) {
       throw new Error("Test failed: 1 + 1 did not equal 2");
     } else {
