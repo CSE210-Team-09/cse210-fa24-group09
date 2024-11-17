71fff1e6f115517d5db332c6be79a88aca9421e8
l2lee <l2lee@ucsd.edu>
2024-11-16 17:53:05 -0800

    issue #5 example of correct code after fixing

--- source/js/linting.js
+++ source/js/linting.js
@@ -1,13 +1,13 @@
 function foo() {
-    console.log("Hello World")
+  console.log('Hello World');
 }
 
 /**
  * Logs "Hello World" to the console
  */
 function hello() {
-    console.log('Hello World');
+  console.log('Hello World');
 }
 
-
+hello();
 
