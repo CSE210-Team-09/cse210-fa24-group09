5a1ff99470e48d80211a2a02e2b09492d0bd5fb9
Juhak Lee <juhak@evt.ai>
2024-11-21 19:49:18 -0800

    Issue #39, #40: Fix linting issue

--- source/js/home.js
+++ source/js/home.js
@@ -1,37 +1,37 @@
 // Sample data
 const notes = [
-  { id: 1, title: "DSA1" },
-  { id: 2, title: "UI Design" },
-  { id: 3, title: "JavaScript Basics"},
-  { id: 4, title: "DSA" },
-  { id: 5, title: "UI Design" },
-  { id: 6, title: "JavaScript Basics"},
-  { id: 7, title: "DSA" },
-  { id: 8, title: "UI Design" },
-  { id: 9, title: "JavaScript Basics"},
-  { id: 10, title: "DSA" },
-  { id: 11, title: "UI Design" },
-  { id: 12, title: "JavaScript Basics"},
-  { id: 13, title: "DSA" },
-  { id: 14, title: "UI Design" },
-  { id: 15, title: "JavaScript Basics"},
-  { id: 21, title: "DSA" },
-  { id: 22, title: "UI Design" },
-  { id: 23, title: "JavaScript Basics"},
-  { id: 24, title: "DSA" },
-  { id: 25, title: "UI Design" },
-  { id: 26, title: "JavaScript Basics"},
-  { id: 27, title: "DSA" },
-  { id: 28, title: "UI Design" },
-  { id: 29, title: "JavaScript Basics"},
-  { id: 210, title: "DSA" },
-  { id: 211, title: "UI Design" },
-  { id: 212, title: "JavaScript Basics"},
-  { id: 213, title: "DSA" },
-  { id: 214, title: "UI Design" },
-  { id: 215, title: "JavaScript Basics"},
-
+  {id: 1, title: 'DSA1'},
+  {id: 2, title: 'UI Design'},
+  {id: 3, title: 'JavaScript Basics'},
+  {id: 4, title: 'DSA'},
+  {id: 5, title: 'UI Design'},
+  {id: 6, title: 'JavaScript Basics'},
+  {id: 7, title: 'DSA'},
+  {id: 8, title: 'UI Design'},
+  {id: 9, title: 'JavaScript Basics'},
+  {id: 10, title: 'DSA'},
+  {id: 11, title: 'UI Design'},
+  {id: 12, title: 'JavaScript Basics'},
+  {id: 13, title: 'DSA'},
+  {id: 14, title: 'UI Design'},
+  {id: 15, title: 'JavaScript Basics'},
+  {id: 21, title: 'DSA'},
+  {id: 22, title: 'UI Design'},
+  {id: 23, title: 'JavaScript Basics'},
+  {id: 24, title: 'DSA'},
+  {id: 25, title: 'UI Design'},
+  {id: 26, title: 'JavaScript Basics'},
+  {id: 27, title: 'DSA'},
+  {id: 28, title: 'UI Design'},
+  {id: 29, title: 'JavaScript Basics'},
+  {id: 210, title: 'DSA'},
+  {id: 211, title: 'UI Design'},
+  {id: 212, title: 'JavaScript Basics'},
+  {id: 213, title: 'DSA'},
+  {id: 214, title: 'UI Design'},
+  {id: 215, title: 'JavaScript Basics'},
 ];
+
 
 // Function to display notes on the homepage
 function displayNotes(filteredNotes = notes) {
@@ -69,16 +69,13 @@ document.getElementById("search-bar").addEventListener("keypress", function (eve
   }
 });
 
-// Placeholder function for viewing a note
 function viewNoteDetails(noteId) {
-  //TODO: integrate view note funtionality
-  alert(`Viewing details for note ID: ${noteId}`);
+  window.location.href = `/note/${noteId}`;
 }
 
 // Placeholder function for creating a new note
 function createNewNote() {
-  //TODO: implement create nod
-  alert("Redirecting to Create New Note page...");
+  window.location.href = `/create`;
 }
 
 // Initial display
