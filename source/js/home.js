918342bb39565bfb92a1d09dd9765b8b42c3fb29
Juhak Lee <juhak@evt.ai>
2024-11-22 15:41:12 -0800

    Issue #52: Integrate data and home page

--- source/js/home.js
+++ source/js/home.js
@@ -1,40 +1,12 @@
 // Sample data
-const notes = [
-  {id: 1, title: 'DSA1'},
-  {id: 2, title: 'UI Design'},
-  {id: 3, title: 'JavaScript Basics'},
-  {id: 4, title: 'DSA'},
-  {id: 5, title: 'UI Design'},
-  {id: 6, title: 'JavaScript Basics'},
-  {id: 7, title: 'DSA'},
-  {id: 8, title: 'UI Design'},
-  {id: 9, title: 'JavaScript Basics'},
-  {id: 10, title: 'DSA'},
-  {id: 11, title: 'UI Design'},
-  {id: 12, title: 'JavaScript Basics'},
-  {id: 13, title: 'DSA'},
-  {id: 14, title: 'UI Design'},
-  {id: 15, title: 'JavaScript Basics'},
-  {id: 21, title: 'DSA'},
-  {id: 22, title: 'UI Design'},
-  {id: 23, title: 'JavaScript Basics'},
-  {id: 24, title: 'DSA'},
-  {id: 25, title: 'UI Design'},
-  {id: 26, title: 'JavaScript Basics'},
-  {id: 27, title: 'DSA'},
-  {id: 28, title: 'UI Design'},
-  {id: 29, title: 'JavaScript Basics'},
-  {id: 210, title: 'DSA'},
-  {id: 211, title: 'UI Design'},
-  {id: 212, title: 'JavaScript Basics'},
-  {id: 213, title: 'DSA'},
-  {id: 214, title: 'UI Design'},
-  {id: 215, title: 'JavaScript Basics'},
-];
 
+API.init(true);
+
+const notes = API.get_all_journals()
 
 // Function to display notes on the homepage
 function displayNotes(filteredNotes = notes) {
+
   const notesList = document.getElementById('notes-list');
   notesList.innerHTML = '';
 
