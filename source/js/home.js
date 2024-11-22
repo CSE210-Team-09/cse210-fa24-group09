2f1c5b70c715d04b05f465c72ffbb181b9b9bba3
Juhak Lee <juhak@evt.ai>
2024-11-21 19:56:56 -0800

    Issue #39, #40: fix linting errors

--- source/js/home.js
+++ source/js/home.js
@@ -35,16 +35,16 @@ const notes = [
 
 // Function to display notes on the homepage
 function displayNotes(filteredNotes = notes) {
-  const notesList = document.getElementById("notes-list");
-  notesList.innerHTML = "";
+  const notesList = document.getElementById('notes-list');
+  notesList.innerHTML = '';
 
   if (filteredNotes.length === 0) {
-    notesList.innerHTML = "<li>No notes found</li>";
+    notesList.innerHTML = '<li>No notes found</li>';
     return;
   }
 
-  filteredNotes.forEach(note => {
-    const noteItem = document.createElement("li");
+  filteredNotes.forEach((note) => {
+    const noteItem = document.createElement('li');
     noteItem.textContent = `${note.title}`;
     noteItem.onclick = () => viewNoteDetails(note.id);
     notesList.appendChild(noteItem);
@@ -53,18 +53,19 @@ function displayNotes(filteredNotes = notes) {
 
 // Function to filter notes based on the search bar input
 function filterNotes() {
-  const searchTerm = document.getElementById("search-bar").value.toLowerCase();
+  const searchTerm = document.getElementById('search-bar').value.toLowerCase();
 
-  const filteredNotes = notes.filter(note =>
-    note.title.toLowerCase().includes(searchTerm)
+  const filteredNotes = notes.filter((note) =>
+    note.title.toLowerCase().includes(searchTerm),
   );
 
   displayNotes(filteredNotes);
 }
 
 // Add listener for the Enter key in the search bar
-document.getElementById("search-bar").addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
+document.getElementById('search-bar').addEventListener('keypress',
+  function(event) {
+  if (event.key === 'Enter') {
     filterNotes();
   }
 });
