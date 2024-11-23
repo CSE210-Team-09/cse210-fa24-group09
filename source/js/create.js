92df9fb00b2bb722419bb579bb78d0ec5642048d
Brandon <bolmos@ucsd.edu>
2024-11-23 15:53:57 -0800

    Issue #42: Added JS code to create the note using the data.js JournalStorage API.

--- source/js/create.js
+++ source/js/create.js
@@ -8,4 +8,13 @@ function saveAndReturn() {
     const comment = document.getElementById('comment-input').value;
 
     // Need to call create_journal from data.js to pass the information over
+    if(API.create_journal(title, code, comment))
+    {
+        alert("Note created successfully!");
+        goHome();
+    }
+    else
+    {
+        alert("Failed to create note.");
+    }
 }
