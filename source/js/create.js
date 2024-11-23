e6da087f02b76c16527a8d5faaf6b04408ead72a
JayGorantla <jayanthg2003@gmail.com>
2024-11-22 22:16:44 -0800

    html/css/js progress

--- source/js/create.js
+++ source/js/create.js
@@ -1,1 +1,11 @@
-// placeholder
+function goHome() {
+    window.location.href = '../html/home.html';
+}
+
+function saveAndReturn() {
+    const title = document.getElementById('text-input').value;
+    const code = document.getElementById('code-input').value;
+    const comment = document.getElementById('comment-input').value;
+
+    // Need to call create_journal from data.js to pass the information over
+}
