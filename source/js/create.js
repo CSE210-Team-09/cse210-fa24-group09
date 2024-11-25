3bfcebda3901e9463b0eca3894ceb31a5b2ea56a
Juhak Lee <jul048@ucsd.edu>
2024-11-24 18:03:08 -0800

    Issue #52: Fix linting issue

--- source/js/create.js
+++ source/js/create.js
@@ -14,7 +14,6 @@ function saveAndReturn() {
   const code = document.getElementById('code-input').value;
   const comment = document.getElementById('comment-input').value;
 
-  
   // Need to call create_journal from data.js to pass the information over
   if (API.create_journal(title, code, comment)) {
     alert('Note created successfully!');
