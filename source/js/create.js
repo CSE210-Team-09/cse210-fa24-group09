5f3dfd3b139b6ac6fb5889e8d58400d22b3a68dd
Juhak Lee <juhak@evt.ai>
2024-12-03 20:36:52 -0800

    Issue #98: Input validation for title in create

--- source/js/create.js
+++ source/js/create.js
@@ -1,6 +1,3 @@
-/**
- * This function routes back to the home page.
- */
 function goHome() {
   window.location.href = '../html/home.html';
 }
@@ -10,16 +7,24 @@ function goHome() {
  * alert if the note is saved successfully, or if an error occurred.
  */
 function save() {
-  const title = document.getElementById('text-input').value;
+  let title = document.getElementById('text-input').value.trim();
   const code = document.getElementById('code-input').value;
   const comment = document.getElementById('comment-input').value;
 
   const tags = document.getElementById('tag-input').value;
   const tagsArr = tags && tags.trim() !== '' ? tags.split(',').map((tag) => tag.trim()) : [];
 
+  // Input validation for the title
+  if (title === '') {
+    title = 'Untitled'; // Default title
+  } else if (title.length > 40) {
+    alert('Title cannot exceed 40 characters. Please shorten your title.');
+    return; // Stop the save function if the title is too long
+  }
+
   // Need to call create_journal from data.js to pass the information over
   if (API.create_journal(title, code, comment, tagsArr)) {
-    noteId = DataIDGenerator.get_data_index() - 1;
+    const noteId = DataIDGenerator.get_data_index() - 1;
     window.location.href = `../html/view.html?id=${noteId}`;
   }
 }
